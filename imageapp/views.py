adea0a74f2b1161aef34876ccebf8a7b2d60b1b6
chakresh3894 <chakresh3894@gmail.com>
2022-03-01 07:24:45 -0500

    done

---
+++ imageapp/views.py
@@ -0,0 +1,27 @@
+from lzma import FORMAT_AUTO
+from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ImageForm
+from .models import Image
+
+# Create your views here.
+
+
+def index(request):
+    if request.method == "POST":
+        form = ImageForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            obj = form.instance
+            
+            return render(request, "index.html", {"obj": obj})
+    else:
+        form = ImageForm()
+        img = Image.objects.all()
+        return render(request, "index.html", {"img": img, "form": form})
+
+def image(request):
+     form = ImageForm()
+     img = Image.objects.all()
+     return render(request,"image.html", {"img": img,})
+ 
